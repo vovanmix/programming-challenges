a1bc3048c8fe3c18d2ad193a43f3900dc7db7cf0
Vladimir Mikhaylovskiy <singingcode@gmail.com>
2017-10-10 15:43:51 -0700

    Update gol.js

--- game-of-life/gol.js
+++ game-of-life/gol.js
@@ -2,96 +2,102 @@
 More hardcore example with row-by-row processing
 */
 
-let row1 = null;
-let row2 = null;
+function GameOfLifeStep() {
+    this.row1 = null;
+    this.row2 = null;
 
-function getNextRow(boardRow) {
-  let newRow = [];
-  
-  if (!row1) {
-    row1 = boardRow;
-    return null;
-  } else if(!row2) {
-    row2 = boardRow;
-    
-    for(let c = 0; c <= row1.length - 1; c++) {
-      newRow[c] = newCellState([row1, row2], 0, c);
-    }
-      
-    row1 = row2;
-  } else if(!boardRow) {
-    for(let c = 0; c <= row1.length - 1; c++) {
-      newRow[c] = newCellState([row1, row2], 1, c);
-    }
-  } else {
-    let row3 = boardRow;
-    for(let c = 0; c <= row1.length - 1; c++) {
-      newRow[c] = newCellState([row1, row2, row3], 1, c);
-    }
-    row1 = row2;
-    row2 = row3;
-  }
-  
-  return newRow;
-}
+    this.getNextRow = function (boardRow) {
+      let newRow = [];
 
-function newCellState(boardState, row, col) {
-  const aliveN = aliveNeighbours(boardState, row, col);
-  
-  if (boardState[row][col] == 1) {
-    if (aliveN < 2) { return 0; }
-    if (aliveN > 3) { return 0; }
-    return 1;
-  } else {
-    if (aliveN == 3) { return 1; }
-    return 0;
-  }
-}
+      if (!this.row1) {
+        this.row1 = boardRow;
+        return null;
+      } else if(!this.row2) {
+        this.row2 = boardRow;
 
-function aliveNeighbours(boardState, row, col) {
-  let alive = 0;
+        for(let c = 0; c <= this.row1.length - 1; c++) {
+          newRow[c] = this.newCellState([this.row1, this.row2], 0, c);
+        }
 
-  for(let r = row - 1; r <= row + 1; r++) {
-    for(let c = col - 1; c <= col + 1; c++) {
-      if (r < 0 || c < 0) { continue; }
-      if (r >= boardState.length || c >= boardState[row].length) { continue; }
-      if (r == row && c == col) { continue; }
-      
-      alive += boardState[r][c];
-    }
-  }
-  return alive;
-}
-
-
-function runStep(board) {
-    row1 = null;
-    row2 = null;
-
-    let newBoard = [];
-    for (let r = 0; r <= board.length; r ++) {
-      if(r == 0) {
-        getNextRow(board[r]);
-      } else if(r == board.length) {
-        newBoard[r - 1] = getNextRow(null);
+        this.row1 = this.row2;
+      } else if(!boardRow) {
+        for(let c = 0; c <= this.row1.length - 1; c++) {
+          newRow[c] = this.newCellState([this.row1, this.row2], 1, c);
+        }
       } else {
-        newBoard[r - 1] = getNextRow(board[r]);
+        let row3 = boardRow;
+        for(let c = 0; c <= this.row1.length - 1; c++) {
+          newRow[c] = this.newCellState([this.row1, this.row2, row3], 1, c);
+        }
+        this.row1 = this.row2;
+        this.row2 = row3;
+      }
+
+      return newRow;
+    }
+
+    this.newCellState = function (boardState, row, col) {
+      const aliveN = this.aliveNeighbours(boardState, row, col);
+
+      if (boardState[row][col] == 1) {
+        if (aliveN < 2) { return 0; }
+        if (aliveN > 3) { return 0; }
+        return 1;
+      } else {
+        if (aliveN == 3) { return 1; }
+        return 0;
       }
     }
-    return newBoard;
+
+    this.aliveNeighbours = function (boardState, row, col) {
+      let alive = 0;
+
+      for(let r = row - 1; r <= row + 1; r++) {
+        for(let c = col - 1; c <= col + 1; c++) {
+          if (r < 0 || c < 0) { continue; }
+          if (r >= boardState.length || c >= boardState[row].length) { continue; }
+          if (r == row && c == col) { continue; }
+
+          alive += boardState[r][c];
+        }
+      }
+      return alive;
+    }
 }
 
-function iterate(steps, board) {
-    if(steps < 0) { return; }
-
-    const newBoard = runStep(board);
-    newBoard.forEach(row => {
-        console.log(row.join(" "));
-    });
-    console.log("-----------");
+function GameOfLife(board) {
+    this.board = board;
     
-    iterate(steps - 1, newBoard);
+    this.runStep = function () {
+        const step = new GameOfLifeStep();
+
+        let newBoard = [];
+        for (let r = 0; r <= this.board.length; r ++) {
+          if(r == 0) {
+            step.getNextRow(this.board[r]);
+          } else if(r == this.board.length) {
+            newBoard[r - 1] = step.getNextRow(null);
+          } else {
+            newBoard[r - 1] = step.getNextRow(this.board[r]);
+          }
+        }
+        return newBoard;
+    }
+
+    this.iterate = function (steps) {
+        if(steps < 0) { return; }
+
+        this.board = this.runStep();
+        this.board.forEach(row => {
+            console.log(row.join(" "));
+        });
+        console.log("-----------");
+
+        this.iterate(steps - 1);
+    }
 }
+
+
 
 // --------------
 
@@ -104,6 +110,7 @@ const board1 = [
   [1,1,1,1,1],
 ];
 
-iterate(10, board1);
+const game = new GameOfLife(board1);
+game.iterate(10);
 
 console.log("done");
