9d7a90cdd71468a9bc17992da47dd61e2314c25f
Vladimir Mikhaylovskiy <singingcode@gmail.com>
2017-10-17 01:21:06 -0700

    Update ph.rb

--- paint-houses/ph.rb
+++ paint-houses/ph.rb
@@ -1,4 +1,14 @@
-#!/usr/bin/env ruby
+=begin
+Find min cost of painting all houses such no adjacent houses have the same color.
+
+Bottom-up greedy:
+- construct a "matrix" where each cell is the best result so far
+- for the 1st row, each cell's value is the price
+- for each row the value is the cost + min of the prev results, not counting the same color
+- the answer is min of the last col
+=end
+
+
 # @param {Integer[][]} costs
 # @return {Integer}
 def min_cost(costs)
